bb129372dec74059f6c304164e3e6beb5fb7c8fe
Sami Sarinc <samisarinc@gmail.com>
2011-11-07 17:50:20 +0100

    moved encoder to util from post class

--- src/org/addhen/smssync/net/MainHttpClient.java
+++ src/org/addhen/smssync/net/MainHttpClient.java
@@ -120,7 +120,7 @@ public class MainHttpClient {
 	public static boolean postSmsToWebService(String url, String xml, Context context) {
 		// Create a new HttpClient and Post Header
 		HttpPost httppost = new HttpPost(url);
-		String encoding = encode("admin:district".getBytes());
+		String encoding = Util.base64encode("admin:district".getBytes());
 		httppost.setHeader("Authorization", "Basic " + encoding);
 
 		try {
@@ -216,41 +216,4 @@ public class MainHttpClient {
 			return null;
 		}
 	}
-
-	private static String encode(byte[] d)
-	{
-		if (d == null) return null;
-		byte data[] = new byte[d.length+2];
-		System.arraycopy(d, 0, data, 0, d.length);
-		byte dest[] = new byte[(data.length/3)*4];
-
-		// 3-byte to 4-byte conversion
-		for (int sidx = 0, didx=0; sidx < d.length; sidx += 3, didx += 4)
-		{
-			dest[didx]   = (byte) ((data[sidx] >>> 2) & 077);
-			dest[didx+1] = (byte) ((data[sidx+1] >>> 4) & 017 |
-					(data[sidx] << 4) & 077);
-			dest[didx+2] = (byte) ((data[sidx+2] >>> 6) & 003 |
-					(data[sidx+1] << 2) & 077);
-			dest[didx+3] = (byte) (data[sidx+2] & 077);
-		}
-
-		// 0-63 to ascii printable conversion
-		for (int idx = 0; idx <dest.length; idx++)
-		{
-			if (dest[idx] < 26)     dest[idx] = (byte)(dest[idx] + 'A');
-			else if (dest[idx] < 52)  dest[idx] = (byte)(dest[idx] + 'a' - 26);
-			else if (dest[idx] < 62)  dest[idx] = (byte)(dest[idx] + '0' - 52);
-			else if (dest[idx] < 63)  dest[idx] = (byte)'+';
-			else            dest[idx] = (byte)'/';
-		}
-
-		// add padding
-		for (int idx = dest.length-1; idx > (d.length*4)/3; idx--)
-		{
-			dest[idx] = (byte)'=';
-		}
-		return new String(dest);
-	}
-
 }
