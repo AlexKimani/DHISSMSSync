68290d10c32c2404c340ace02f30a9193d1ded97
mustafma <mustafa1982@gmail.com>
2011-11-29 20:10:34 +0100

    Accepting spaces in pair sms format

--- src/org/dhis/smssync/util/DhisConstants.java
+++ src/org/dhis/smssync/util/DhisConstants.java
@@ -5,7 +5,7 @@ import android.os.Environment;
 public class DhisConstants {
 	
 	public static final String PIPED_REGEX = "\\w+#\\w+#((\\w+\\|)*(\\w+))";
-	public static final String PAIR_REGEX = "\\w+\\s+(((\\w+\\.\\w+=\\w+|\\w+=\\w+),\\s*)*((\\w+\\.\\w+=\\w+|\\w+=\\w+)){1})";
+	public static final String PAIR_REGEX = "\\w+\\s+(((\\w+\\.\\w+\\s*=\\s*\\w+|\\w+\\s*=\\s*\\w+),\\s*)*((\\w+\\.\\w+\\s*=\\s*\\w+|\\w+\\s*=\\s*\\w+)){1})";
 	
 	public static final String EXTERNAL_STORAGE = Environment.getExternalStorageDirectory() + "";
 	public static final String EXPORT_DIRECTORY = EXTERNAL_STORAGE + "/DHIS_EXPORT/";
